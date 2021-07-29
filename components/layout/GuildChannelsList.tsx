123f83b0a0ee52406f90ddbb0201a54a334c6a0e
Nilomiranda <nilomiranda3@gmail.com>
2021-07-28 22:57:11 -0300

    loading server channels

--- components/layout/GuildChannelsList.tsx
+++ components/layout/GuildChannelsList.tsx
@@ -3,25 +3,24 @@ import ServerButton from "../ServerButton";
 import {VStack} from "@chakra-ui/react";
 import {useQuery} from "react-query";
 import {useEffect, useState} from "react";
+import ChannelButton from "../ChannelButton";
 
 interface GuildChannelsListProps {
   guildId: string
 }
 
 const GuildChannelsList = ({ guildId }: GuildChannelsListProps) => {
-  const [channels, setChannels] = useState<GuildChannel[]>([])
+  // const [channels, setChannels] = useState<GuildChannel[]>([])
+  const { data: channelsData } = useQuery<{ channels: GuildChannel[] }>(`discord/channels?guildId=${guildId}`, { enabled: !!guildId })
+
 
   if (!guildId) {
     return null
   }
 
-  if (!channels?.length) {
-    return null
-  }
-
   return (
     <VStack mt="3rem" spacing="1.5rem" maxHeight={"79vh"} overflowY={"scroll"}>
-      { channels?.map(channel => <ChannelButton channel={channel} />) }
+      { channelsData?.channels?.length ? channelsData?.channels?.map(channel => <ChannelButton channel={channel} key={channel?.id} />) : null }
     </VStack>
   )
 }
