8016d17eb4d6ef74eb499f83dbdac3a6e28df232
Nilomiranda <nilomiranda3@gmail.com>
2021-07-30 18:15:07 -0300

    clearing local storage on logout

--- components/layout/Sidebar/SidebarUser.tsx
+++ components/layout/Sidebar/SidebarUser.tsx
@@ -1,11 +1,11 @@
-import {useContext} from "react";
-import {Avatar, Box, Button, Text, Spinner, useToast} from "@chakra-ui/react";
+import { useContext } from 'react'
+import { Avatar, Box, Button, Text, Spinner, useToast } from '@chakra-ui/react'
 import { IoLogOutOutline } from 'react-icons/io5'
-import {useRouter} from "next/router";
-import {UserContext} from "../../../contexts/CurrentUser";
-import {DISCORD_USER_AVATAR_CDN_BASE_URL, TOAST_DEFAULT_DURATION} from "../../../config/constants";
-import {logout} from "../../../services/auth";
-import {getUserAvatar} from "../../../interfaces/user";
+import { useRouter } from 'next/router'
+import { UserContext } from '../../../contexts/CurrentUser'
+import { TOAST_DEFAULT_DURATION } from '../../../config/constants'
+import { logout } from '../../../services/auth'
+import { getUserAvatar } from '../../../interfaces/user'
 
 const SidebarUser = () => {
   const toast = useToast()
@@ -15,11 +15,12 @@ const SidebarUser = () => {
   const handleLogOut = async () => {
     try {
       await logout()
+      window?.localStorage?.clear()
       toast({
         status: 'success',
         isClosable: true,
         duration: TOAST_DEFAULT_DURATION,
-        description: 'Logged out'
+        description: 'Logged out',
       })
       router.push('/login')
     } catch (err) {
@@ -27,31 +28,38 @@ const SidebarUser = () => {
         status: 'error',
         isClosable: true,
         duration: TOAST_DEFAULT_DURATION,
-        description: 'Could not log you out'
+        description: 'Could not log you out',
       })
     }
   }
 
   if (!user) {
     return (
-      <Box padding={"1rem"}>
-        <Spinner color={"white"} />
+      <Box padding="1rem">
+        <Spinner color="white" />
       </Box>
     )
   }
 
   return (
-    <Box padding={"1rem"} display={"flex"} flexDirection={"column"} alignItems={"flex-start"}>
-      <Box display={"flex"} alignItems={"flex-start"}>
-        <Avatar name={discordUser?.username} src={getUserAvatar(discordUser)}  />
+    <Box padding="1rem" display="flex" flexDirection="column" alignItems="flex-start">
+      <Box display="flex" alignItems="flex-start">
+        <Avatar name={discordUser?.username} src={getUserAvatar(discordUser)} />
 
-        <Box display={"flex"} flexDirection={"column"} alignItems={"flex-start"} ml={"1rem"}>
-          <Text color={"white"} fontSize={"xl"}>{discordUser?.username} <Text as={"span"} fontSize={"sm"} color={"gray.600"}>({user?.guildName})</Text></Text>
-          <Text color={"white"}>{user?.email}</Text>
+        <Box display="flex" flexDirection="column" alignItems="flex-start" ml="1rem">
+          <Text color="white" fontSize="xl">
+            {discordUser?.username}{' '}
+            <Text as="span" fontSize="sm" color="gray.600">
+              ({user?.guildName})
+            </Text>
+          </Text>
+          <Text color="white">{user?.email}</Text>
         </Box>
       </Box>
 
-      <Button size={"sm"} mt={"1.5rem"} leftIcon={<IoLogOutOutline />} colorScheme={"blackAlpha"} onClick={handleLogOut}>Log out</Button>
+      <Button size="sm" mt="1.5rem" leftIcon={<IoLogOutOutline />} colorScheme="blackAlpha" onClick={handleLogOut}>
+        Log out
+      </Button>
     </Box>
   )
 }
