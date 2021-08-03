920d99e6a2a9a2662f3fe7ea5f99b96902645409
Nilomiranda <nilomiranda3@gmail.com>
2021-08-02 21:25:41 -0300

    simple ts fix

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,9 +1,9 @@
-import {Box, Button, useToast} from "@chakra-ui/react";
-import {useRouter} from "next/router";
-import {useState} from "react";
-import {TOAST_DEFAULT_DURATION} from "../config/constants";
-import Input from "../components/form/Input";
-import {login} from "../services/auth";
+import { Box, Button, useToast } from '@chakra-ui/react'
+import { useRouter } from 'next/router'
+import { useState } from 'react'
+import { TOAST_DEFAULT_DURATION } from '../config/constants'
+import Input from '../components/form/Input'
+import { login } from '../services/auth'
 
 const LoginPage = () => {
   const toast = useToast()
@@ -12,7 +12,7 @@ const LoginPage = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
-  const handleLoginSubmit = async (event: Event) => {
+  const handleLoginSubmit = async (event) => {
     event?.preventDefault()
     setIsAuthenticating(true)
 
@@ -26,7 +26,7 @@ const LoginPage = () => {
         description: err?.response?.data?.errors || 'An unexpected error occurred.',
         status: 'error',
         duration: TOAST_DEFAULT_DURATION,
-        isClosable: true
+        isClosable: true,
       })
     } finally {
       setIsAuthenticating(false)
@@ -35,16 +35,18 @@ const LoginPage = () => {
 
   return (
     <Box h="100vh" w="100%" display="flex" alignItems="center" justifyContent="center" backgroundColor="gray.800">
-      <Box as="form" display="flex" flexDirection="column" w={"100%"} maxWidth={"400px"} px="1rem" onSubmit={handleLoginSubmit}>
-        <Box mb={"1rem"}>
-          <Input label={"Email"} placeholder={"email"} type={"email"} id={"email"} value={email} onChange={(event) => setEmail(event?.target?.value)} />
+      <Box as="form" display="flex" flexDirection="column" w="100%" maxWidth="400px" px="1rem" onSubmit={handleLoginSubmit}>
+        <Box mb="1rem">
+          <Input label="Email" placeholder="email" type="email" id="email" value={email} onChange={(event) => setEmail(event?.target?.value)} />
         </Box>
 
-        <Box mb={"1rem"}>
-          <Input label={"Password"} placeholder={"password"} type={"password"} id={"password"} value={password} onChange={(event) => setPassword(event?.target?.value)} />
+        <Box mb="1rem">
+          <Input label="Password" placeholder="password" type="password" id="password" value={password} onChange={(event) => setPassword(event?.target?.value)} />
         </Box>
 
-        <Button mt={"1.5rem"} type="submit" colorScheme="pink" isLoading={isAuthenticating}>Login</Button>
+        <Button mt="1.5rem" type="submit" colorScheme="pink" isLoading={isAuthenticating}>
+          Login
+        </Button>
       </Box>
     </Box>
   )
