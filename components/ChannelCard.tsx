46c3e3cf26ca8132020c0474b7883218dc494ccf
Nilomiranda <nilomiranda3@gmail.com>
2021-08-10 19:56:34 -0300

    sorting messages by newest at the top by default and added sorting button to channel cards

--- components/ChannelCard.tsx
+++ components/ChannelCard.tsx
@@ -1,4 +1,4 @@
-import { Box, Heading, VStack, CloseButton } from '@chakra-ui/react'
+import { Box, Heading, VStack, CloseButton, Button } from '@chakra-ui/react'
 import { useQuery, useQueryClient } from 'react-query'
 import { useEffect, useState } from 'react'
 import { GuildChannel } from '../interfaces/guildChannel'
@@ -19,6 +19,7 @@ const ChannelCard = ({ channel }: ChannelCardProps) => {
   const [messages, setMessages] = useState<ChannelMessageType[]>([])
   const [isMessageModalOpen, setIsMessageModalOpen] = useState<boolean>(false)
   const [selectedMessage, setSelectedMessage] = useState<ChannelMessageType>(null)
+  const [currentSorting, setCurrentSorting] = useState<'newest' | 'oldest'>('newest')
 
   const handleMessageClick = (message: ChannelMessageType) => {
     setSelectedMessage(message)
@@ -30,9 +31,19 @@ const ChannelCard = ({ channel }: ChannelCardProps) => {
     setSelectedMessage(null)
   }
 
+  const handleSortClick = () => {
+    const newSortingState = currentSorting === 'newest' ? 'oldest' : 'newest'
+    setCurrentSorting(newSortingState)
+    setMessages(messageData?.messages?.reverse())
+  }
+
   useEffect(() => {
     if (messageData?.messages?.length) {
-      setMessages(messageData?.messages?.reverse())
+      if (currentSorting === 'oldest') {
+        setMessages(messageData?.messages?.reverse())
+      } else {
+        setMessages(messageData?.messages)
+      }
     }
   }, [messageData])
 
@@ -54,9 +65,14 @@ const ChannelCard = ({ channel }: ChannelCardProps) => {
     <>
       <Box h="100%" width="400px" overflowX="hidden" overflowY="hidden">
         <Box display="flex" alignItems="center" justifyContent="space-between" w="400px" bg="gray.900" py="0.5rem">
-          <Heading fontSize="lg" color="gray.600">
-            {channel.name}
-          </Heading>
+          <Box display="flex" alignItems="center">
+            <Heading fontSize="lg" color="gray.600" mr="0.5rem">
+              {channel.name}
+            </Heading>
+            <Button colorScheme="gray" variant="link" fontSize="sm" onClick={handleSortClick}>
+              Sort by {currentSorting === 'newest' ? 'oldest' : 'newest'}
+            </Button>
+          </Box>
           <CloseButton color="red" onClick={() => removeChannelFromList(channel?.id)} />
         </Box>
 
