f72d3485df451c9a683bb12b8a2106b82118807b
Nilomiranda <nilomiranda3@gmail.com>
2021-08-20 22:22:05 -0300

    filtering for hidden channels

--- components/layout/GuildChannelsList.tsx
+++ components/layout/GuildChannelsList.tsx
@@ -1,4 +1,4 @@
-import { Box, VStack } from '@chakra-ui/react'
+import { Box, VStack, Text } from '@chakra-ui/react'
 import { useQuery } from 'react-query'
 import { useContext, useEffect, useState } from 'react'
 import { GuildChannel } from '../../interfaces/guildChannel'
@@ -6,9 +6,12 @@ import ChannelButton from '../ChannelButton'
 import SearchInput from '../form/SearchInput'
 import { UserContext } from '../../contexts/CurrentUser'
 import {useSelectedChannelsStore} from "../../stores/selectedChannelsStore";
+import {useHiddenChannelsStore} from "../../stores/visibleChannelsStore";
 
 const GuildChannelsList = () => {
   const addChannelToList = useSelectedChannelsStore(state => state?.addChannelToList)
+  const hiddenChannelsIds = useHiddenChannelsStore(state => state?.channels)?.map(hiddenChannel => hiddenChannel?.id)
+
   const { user } = useContext(UserContext)
   const { data: channelsData } = useQuery<{ channels: GuildChannel[] }>(`discord/channels?guildId=${user?.guildID}`, { enabled: !!user?.guildID })
   const [channelsOriginalList, setChannelsOriginalLit] = useState<GuildChannel[]>([])
@@ -32,13 +35,15 @@ const GuildChannelsList = () => {
   }, [channelsData])
 
   return (
-    <VStack mt="3rem" spacing="1.5rem" maxHeight="79vh" overflowY="auto">
+    <VStack mt="3rem" spacing="1.5rem" maxHeight="70vh" overflowY="auto">
       <Box px="1.25rem" w="100%">
         <SearchInput onSearchChange={handleChannelsSearch} placeholder="Search channel" />
+        { hiddenChannelsIds?.length ? <Text mt="1rem" color={"white"} fontSize={"xs"} color={"gray.600"}>Some channels are currently hidden</Text> : null }
       </Box>
 
       {channels
         ?.filter((channel) => channel?.type === 0)
+        ?.filter((channel) => !hiddenChannelsIds?.includes(channel?.id))
         ?.map((channel) => (
           <ChannelButton channel={channel} key={channel?.id} onChannelClick={handleChannelClicked} />
         ))}
